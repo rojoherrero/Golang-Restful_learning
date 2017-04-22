a7e74d95ad28db2de9fff6091808d9ccd3554b85
rojoherrero <rojoherrero@gmail.com>
2017-04-22 17:16:37 +0200

    V2

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -13,9 +15,18 @@ const (
 )
 
 func main() {
-	log.Print("Running server on " + PORT)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
-	})
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", sayHello)
+	router.HandleFunc("/{name}", sayHelloTo)
+	log.Println(http.ListenAndServe(PORT, router))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %q, this is version 2", html.EscapeString(r.URL.Path))
+}
+
+func sayHelloTo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	fmt.Fprintln(w, "Hello,", name)
 }
