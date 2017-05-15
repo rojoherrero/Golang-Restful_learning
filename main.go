7edf0061f683c7589b17cd21f6748bd636d589d5
rojoherrero <rojoherrero@gmail.com>
2017-05-15 19:21:35 +0200

    my first api rest in golang

--- main.go
+++ main.go
@@ -1,32 +1,92 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-const (
-	// PORT port
-	PORT = "127.0.0.1:8080"
-)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the Home Page")
+	log.Println("Endpotin Hit: homePage")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", sayHello)
-	router.HandleFunc("/{name}", sayHelloTo)
-	log.Println(http.ListenAndServe(PORT, router))
+
+	handleRequests()
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %q, this is version 2", html.EscapeString(r.URL.Path))
+type Article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
-func sayHelloTo(w http.ResponseWriter, r *http.Request) {
+type Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: returnAllArticles")
+	db := mariaDb()
+	defer db.Close()
+	results, err := db.Query("SELECT * FROM articles")
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	var returnedArticles Articles
+
+	for results.Next() {
+		var article Article
+		err := results.Scan(&article.ID, &article.Title, &article.Desc, &article.Content)
+		if err != nil {
+			log.Print(err.Error())
+		}
+		returnedArticles = append(returnedArticles, article)
+	}
+
+	json.NewEncoder(w).Encode(returnedArticles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: returnSingleArticle")
+	db := mariaDb()
+	defer db.Close()
 	vars := mux.Vars(r)
-	name := vars["name"]
-	fmt.Fprintln(w, "Hello,", name)
+	stringKey := vars["id"]
+	key, _ := strconv.Atoi(stringKey)
+
+	var article Article
+
+	err := db.QueryRow("SELECT * FROM articles WHERE id = ?", key).Scan(&article.ID, &article.Title, &article.Desc, &article.Content)
+
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	json.NewEncoder(w).Encode(article)
+
+}
+
+func mariaDb() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@/golang_test")
+	if err != nil {
+		panic(err.Error)
+	}
+	log.Println("Connection to MariaDB created")
+	return db
 }
---
+++ resources/article_test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO golang_test.articles (id,title,description,content) VALUES 
+(1,'title 1','description 1','content 1')
+,(2,'title 2','description 2','content 2')
+,(3,'title 3','description 3','content 3')
+,(4,'title 4','description 4','content 4')
+,(5,'title 5','description 5','content 5')
+;
